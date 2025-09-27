e2a2f057af30e1e961c4ac2b3a153f0fbb4ba2c4
hazemmasarani <hazemmasarani99@gmail.com>
2025-09-27 18:00:25 -0400

    all-reduce-exp

--- gpu-multiproccess/all-reduce.py
+++ gpu-multiproccess/all-reduce.py
@@ -3,16 +3,25 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 
 def worker(rank, world_size, return_dict):
+    print(f"Rank {rank} starting...")
     # Initialize process group
     dist.init_process_group(
-        backend="gloo",  # "nccl",
+        backend="nccl",
         init_method="tcp://127.0.0.1:29500",
         world_size=world_size,
         rank=rank
     )
 
+    # Define tensor on all ranks
+    A = torch.empty(5, 5, device=f"cuda:{rank}")
+    if rank == 0:
+        A = torch.randn(5, 5, device=f"cuda:0")
+        return_dict["A_orig"] = A.clone().cpu()
+        print(A)
+    
+    dist.broadcast(A, src=0)
+    
     # Each rank computes its own A @ A
-    A = torch.randn(100, 100, device=f"cuda:{rank}")
     A = torch.matmul(A, A)
 
     # All-reduce to compute the sum across ranks
@@ -28,7 +37,7 @@ def worker(rank, world_size, return_dict):
 
 
 def main():
-    world_size = dist.get_world_size()
+    world_size = torch.cuda.device_count() # Number of available GPUs
     manager = mp.Manager()
     return_dict = manager.dict()
 
@@ -36,9 +45,18 @@ def main():
 
     if "A" in return_dict:
         A_master = return_dict["A"]
-        print("Final averaged result on rank 0:", A_master.shape)
-        return A_master
-
+        A_orig = return_dict["A_orig"]
+        print("A_orig from rank 0:\n", A_orig)
+        validate = torch.matmul(A_orig, A_orig)
+        if torch.allclose(A_master, validate):
+            print("Validation successful: A_master matches A_orig @ A_orig")
+        else:
+            print("Validation failed: A_master does not match A_orig @ A_orig")
+        print("Validation result:\n", validate)
+        print("Final averaged result on rank 0:\n", A_master)
+        
+        max_error = torch.max(torch.abs(A_master - validate))
+        print("Maximum absolute error:", max_error.item())
 
 if __name__ == "__main__":
-    A = main()
+    main()
